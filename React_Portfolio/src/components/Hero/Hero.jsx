9943cdfa23f9067e5e17e81a97833aa5a7fcaf8f
eddiespag-hetti <eddie_p89@hotmail.com>
2024-04-25 15:23:03 +1000

    Added img and icons to hero section
    Updates minor styling to Navbar

--- React_Portfolio/src/components/Hero/Hero.jsx
+++ React_Portfolio/src/components/Hero/Hero.jsx
@@ -1,10 +1,46 @@
-
+// Importing relevent CSS styling
 import './Hero.css'
+
+// Hero Component
 const Hero = () => {
     return (
-        <div>
-            Hero
-        </div>
-    )}
+<section className='hero-container'>
+  <div className='hero-content'>
+    <div className='text-content'>
+      <h2>Building Digital Experiences That Inspire</h2>
+      <p>Passionate Full-Stack Developer | Transforming Ideas in Code</p>
+    </div>
+    <div className='tech-icons'>
+      <div className='tech-icon'>
+        <img src="./img/javascript.svg" alt='JavaScript icon' />
+      </div>
+      <div className='tech-icon'>
+        <img src="./img/vite.svg" alt='Vite icon' />
+      </div>
+      <div className='tech-icon'>
+        <img src="./img/nodedotjs.svg" alt='Node.js icon' />
+      </div>
+      <div className='tech-icon'>
+        <img src="./img/html5.svg" alt='HTML5 icon' />
+      </div>
+      <div className='tech-icon'>
+        <img src="./img/css3.svg" alt='CSS3 icon' />
+      </div>
+    </div>
+  </div>
+  <div className='hero-img'>
+    <img className="hero-img" src="./img/hero-img.png" alt='Image of a developers workspace' />
+  </div>
+</section>
+
+
+  
+        
+      
+  
+
+
+)
+};
 
 export default Hero;
